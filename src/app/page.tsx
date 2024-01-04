7700c69d8d12f5834286fe8002aeb3922a4272cb
harshpandey002 <coding.harshp@gmail.com>
2024-01-04 23:20:00 +0530

    Gradients & Shadows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@
 
 import { Button } from '@/components/ui/button';
 import { ArrowDownUp } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form className="max-w-md flex flex-col flex-grow">
+    <main className="relative flex h-screen flex-col items-center justify-between p-24 overflow-hidden">
+      <Image
+        src="/blob.png"
+        width={1000}
+        height={1000}
+        alt="blob"
+        className="absolute top-1/2 left-1/2 z-0 transform -translate-x-1/2 -translate-y-1/2 opacity-40"
+      />
+      <form className="max-w-lg flex flex-col z-10 bg-background p-10 rounded-2xl shadow-[0px_0px_10px_4px_#0000005a]">
         <h3 className="text-2xl font-semibold">Burn $NUMA</h3>
         <p className="mt-1 text-[#545b76]">
           Lorem ipsum dolor sit amet consectetur
@@ -19,6 +27,7 @@ export default function Home() {
             Change Wallet?
           </a>
         </span>
+
         <div className="w-full flex flex-col gap-2 rounded-2xl bg-[#16181fd2] p-4 mb-2">
           <span className="w-full flex items-center justify-between text-sm text-gray-600">
             <p>You pay</p>
@@ -35,9 +44,11 @@ export default function Home() {
             </span>
           </span>
         </div>
-        <span className="w-max h-max p-2 rounded-full bg-[#0d0e12] text-[#3d4154] flex item-center justify-center mx-auto -my-5 z-10 cursor-pointer hover:bg-[#3b4255] hover:text-[#8a90a5]">
+
+        <span className="w-max h-max p-2 rounded-full bg-background text-[#3d4154] flex item-center justify-center mx-auto -my-5 z-10 cursor-pointer hover:bg-[#1b1e25] hover:text-[#8a90a5]">
           <ArrowDownUp />
         </span>
+
         <div className="w-full flex flex-col gap-2 rounded-2xl bg-[#16181fd2] p-4 mb-2">
           <span className="w-full flex items-center justify-between text-sm text-gray-600">
             <p>You receive</p>
@@ -53,6 +64,11 @@ export default function Home() {
               <p>rETH</p>
             </span>
           </span>
+        </div>
+
+        <div className="flex items-center justify-between mt-1 text-[#979fc1]">
+          <p>Price: 45c</p>
+          <p>Fee: 5%</p>
         </div>
 
         <Button
